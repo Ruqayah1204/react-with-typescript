b87c340b8efcfd770ff0608f6aba30842539887f
Ruqayah1204 <mhiedey98@gmail.com>
2025-03-05 16:31:14 +0100

    countdown task commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import AutoSaveInput from "./AutoSaveInput";
 import Countdown from "./Countdown";
-// import ThemeToggle from "./Toggle";
 
 function App() {
   return (
     <>
-      {/* <ThemeToggle /> */}
       <div className="container">
         <h1>Implementing Countdown Timer</h1>
         <p>To decrease number every second</p>
       </div>
       <Countdown />
-    </> // <-- React Fragment
+    </>
   );
 }
 export default App;
